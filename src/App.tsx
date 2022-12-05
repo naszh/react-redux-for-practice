fcc0e8381ca7e7457494f6d21452bfbb61a65afe
Naszh <naszh99@gmail.com>
2022-12-05 13:01:23 +0300

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import { useContext } from 'react';
 import { Wrapper } from './App.styles';
 import { People } from './components/people/people';
 import { SignUpForm } from './components/signup/signup';
+import { ThemeContext } from './components/theme/themeProvider';
 
-function App() {
+const App: React.FC = () => {
+	const { theme, toggleTheme } = useContext(ThemeContext);
 	return (
 		<Wrapper>
 			<SignUpForm />
 			{/* <People /> */}
+			<button onClick={toggleTheme}>
+				Switch to {theme === 'light' ? 'dark' : 'light'} mode
+			</button>
 		</Wrapper>
 	);
-}
+};
 
 export default App;
