bb2b1304f96740a2c8fdaa58295708dc3e92c318
Naszh <naszh99@gmail.com>
2022-12-09 15:35:25 +0300

    change theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { FC, useContext } from 'react';
 import { Wrapper } from './App.styles';
-import { ButtonElement } from './components/common/button/button';
 import { SignInForm } from './components/signin/signin';
 import { SignUpForm } from './components/signup/signup';
 import { ThemeContext } from './components/theme/themeProvider';
@@ -8,28 +7,28 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './components/home/home';
 import { About } from './components/about/about';
 import { People } from './components/people/people';
+import { MaterialUISwitch } from './components/switch/switch';
 
 const App: FC = () => {
-	const { theme, toggleTheme } = useContext(ThemeContext);
+	const { toggleTheme } = useContext(ThemeContext);
+
 	return (
-		<Router>
-			<Routes>
-				<Route path='/main' element={<People />} />
-				<Route path='/home' element={<Home />} />
-				<Route path='/about' element={<About />} />
-			</Routes>
-			<ButtonElement
-				onClick={toggleTheme}
-				text={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
-				size={'small'}
-			/>
-			<Wrapper>
+		<>
+			<MaterialUISwitch onClick={toggleTheme} />
+			<Router>
 				<Routes>
-					<Route path='/' element={<SignUpForm />} />
-					<Route path='/signin' element={<SignInForm />} />
+					<Route path='/main' element={<People />} />
+					<Route path='/home' element={<Home />} />
+					<Route path='/about' element={<About />} />
 				</Routes>
-			</Wrapper>
-		</Router>
+				<Wrapper>
+					<Routes>
+						<Route path='/' element={<SignUpForm />} />
+						<Route path='/signin' element={<SignInForm />} />
+					</Routes>
+				</Wrapper>
+			</Router>
+		</>
 	);
 };
 
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,33 +3,31 @@ import { HeaderContainer } from './header.styles';
 
 export const Header = () => {
 	return (
-		<>
-			<HeaderContainer>
-				<NavLink
-					to={'/home'}
-					style={({ isActive }) =>
-						isActive ? { color: 'yellow' } : { color: 'inherit' }
-					}
-				>
-					<p>Home</p>
-				</NavLink>
-				<NavLink
-					to={'/main'}
-					style={({ isActive }) =>
-						isActive ? { color: 'yellow' } : { color: 'inherit' }
-					}
-				>
-					<p>Main</p>
-				</NavLink>
-				<NavLink
-					to={'/about'}
-					style={({ isActive }) =>
-						isActive ? { color: 'yellow' } : { color: 'inherit' }
-					}
-				>
-					<p>About</p>
-				</NavLink>
-			</HeaderContainer>
-		</>
+		<HeaderContainer>
+			<NavLink
+				to={'/home'}
+				style={({ isActive }) =>
+					isActive ? { color: 'yellow' } : { color: 'inherit' }
+				}
+			>
+				<p>Home</p>
+			</NavLink>
+			<NavLink
+				to={'/main'}
+				style={({ isActive }) =>
+					isActive ? { color: 'yellow' } : { color: 'inherit' }
+				}
+			>
+				<p>Main</p>
+			</NavLink>
+			<NavLink
+				to={'/about'}
+				style={({ isActive }) =>
+					isActive ? { color: 'yellow' } : { color: 'inherit' }
+				}
+			>
+				<p>About</p>
+			</NavLink>
+		</HeaderContainer>
 	);
 };
